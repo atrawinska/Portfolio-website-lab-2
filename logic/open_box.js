4539ddaee5ee03343f235d83a519c02afbfeaf21
atrawinska <agnieszka.alicja.trawinska@gmail.com>
2026-01-05 19:03:57 +0100

    Added opening pages

---
+++ logic/open_box.js
@@ -0,0 +1,17 @@
+
+
+//take the id
+const urlParams = new URLSearchParams(window.location.search);
+const projectId = parseInt(urlParams.get("id")); // convert to number
+
+//find the box
+const project = projects.find(p => p.id === projectId);
+
+//take the data
+if (project) {
+  document.getElementById("project-title").textContent = project.title;
+  document.getElementById("project-image").src = project.image;
+  document.getElementById("project-image").alt = project.title;
+  document.getElementById("project-description").textContent = project.description;
+  document.getElementById("project-technologies").textContent = "Technologies: " + project.technologies.join(", ");
+}
--- logic/projects.js
+++ logic/projects.js
@@ -1,12 +1,12 @@
 
 //array of projects
 const projects = [
-  { id: 1, title: "Autonomous car app", description: "My personal portfolio built with HTML, CSS, and JavaScript", category: "Mobile App", technologies: ["Flutter","Dart","BLE"], image: "/images/Picture1.png", link: "#" },
-  { id: 2, title: "Boat app", description: "A simple todo application", category: "Mobile App", technologies: ["Flutter","Dart","SSH"], image: "/images/Picture2.png", link: "#" },
-  { id: 3, title: "Weather app", description: "A logo I designed for a local company", category: "Mobile App", technologies: ["Flutter", "Dart"], image: "/images/Picture3.png", link: "#" },
-  { id: 4, title: "Social campaign", description: "A group project for school", category: "Social Projects", technologies: ["Instagram","Canva"], image: "/images/Picture4.png", link: "#" },
-  { id: 5, title: "GrowBox app", description: "A landing page for a product", category: "Mobile App", technologies: ["Flutter","Dart", "BLE"], image: "/images/Picture5.png", link: "#" },
-  { id: 6, title: "RF communication", description: "Poster design project", category: "Electronics", technologies: ["ESP32", "ArduinoIDE/C++", "AM modules at 433MHz"], image: "/images/Picture6.png", link: "#" }
+  { id: 1, title: "Autonomous car app", description: "App to monitor the car (ultrasonic sensors' readings, color sensor ect)", category: "Mobile App", technologies: ["Flutter","Dart","BLE"], image: "/images/Picture1.png", link: "opened_box.html?id=1" },
+  { id: 2, title: "Boat app", description: "Boat monitoring system", category: "Mobile App", technologies: ["Flutter","Dart","SSH"], image: "/images/Picture2.png", link: "opened_box.html?id=2" },
+  { id: 3, title: "Weather app", description: "Weather app :)", category: "Mobile App", technologies: ["Flutter", "Dart"], image: "/images/Picture3.png", link: "opened_box.html?id=3" },
+  { id: 4, title: "Social campaign", description: "Social campaign about self-esteem", category: "Social Projects", technologies: ["Instagram","Canva"], image: "/images/Picture4.png", link: "opened_box.html?id=4" },
+  { id: 5, title: "GrowBox app", description: "Mobile app for a plant monitoring", category: "Mobile App", technologies: ["Flutter","Dart", "BLE"], image: "/images/Picture5.png", link: "opened_box.html?id=5" },
+  { id: 6, title: "RF communication", description: "Electronics project for RF communication with TX/FX 433MHz modules", category: "Electronics", technologies: ["ESP32", "ArduinoIDE/C++", "AM modules at 433MHz"], image: "/images/Picture6.png", link: "opened_box.html?id=6" }
 ];
 
 //locate elements form the main doc
