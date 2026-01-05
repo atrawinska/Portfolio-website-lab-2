060b03d583eabf025d30f57f20addaa4549ab27f
atrawinska <agnieszka.alicja.trawinska@gmail.com>
2026-01-05 19:07:37 +0100

    Fixed passing data by adding a common file for data storage project_data.js

--- logic/projects.js
+++ logic/projects.js
@@ -1,13 +1,4 @@
 
-//array of projects
-const projects = [
-  { id: 1, title: "Autonomous car app", description: "App to monitor the car (ultrasonic sensors' readings, color sensor ect)", category: "Mobile App", technologies: ["Flutter","Dart","BLE"], image: "/images/Picture1.png", link: "opened_box.html?id=1" },
-  { id: 2, title: "Boat app", description: "Boat monitoring system", category: "Mobile App", technologies: ["Flutter","Dart","SSH"], image: "/images/Picture2.png", link: "opened_box.html?id=2" },
-  { id: 3, title: "Weather app", description: "Weather app :)", category: "Mobile App", technologies: ["Flutter", "Dart"], image: "/images/Picture3.png", link: "opened_box.html?id=3" },
-  { id: 4, title: "Social campaign", description: "Social campaign about self-esteem", category: "Social Projects", technologies: ["Instagram","Canva"], image: "/images/Picture4.png", link: "opened_box.html?id=4" },
-  { id: 5, title: "GrowBox app", description: "Mobile app for a plant monitoring", category: "Mobile App", technologies: ["Flutter","Dart", "BLE"], image: "/images/Picture5.png", link: "opened_box.html?id=5" },
-  { id: 6, title: "RF communication", description: "Electronics project for RF communication with TX/FX 433MHz modules", category: "Electronics", technologies: ["ESP32", "ArduinoIDE/C++", "AM modules at 433MHz"], image: "/images/Picture6.png", link: "opened_box.html?id=6" }
-];
 
 //locate elements form the main doc
 const projectContainer = document.getElementById("project-container");
